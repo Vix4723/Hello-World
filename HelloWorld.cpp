44e88f6bd1f21b73df2763f29b3bbe0c48bae009
Vix <63135033+Vix4723@users.noreply.github.com>
2021-01-10 14:54:57 -0500

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Victoria Clotet
+// January 10th 2021
+// Local Hack Day: Build 2021 - Hello, World in a New Language Daily Challenge
+// Learning Source: https://devblogs.microsoft.com/cppblog/cpp-tutorial-hello-world/
+
+#include <iostream>
+int main()
+{
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
